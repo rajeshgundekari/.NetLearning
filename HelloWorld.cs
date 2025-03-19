582cb99b95c43e2c06674b644fd1767367bc09a4
rajeshgundekari <67254331+rajeshgundekari@users.noreply.github.com>
2025-03-19 22:18:21 +0530

    Create HelloWorld.cs
    start with learning Helloworld in C#

---
+++ HelloWorld.cs
@@ -0,0 +1,10 @@
+// Hello World Program in C#
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello World|| this is my first C# Program");
+	}
+}
